e9e63b57ee304aa23864537e867efd96420ff28f
creamlaflare <antonkazachenko2002@gmail.com>
2024-09-22 18:47:40 -0700

    update icons

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,9 +7,9 @@ export default function Hero() {
       <h1 className="hero-title">Anton Kazachenko</h1>
       <h2 className="hero-subtitle">Software Developer</h2>
       <p className="hero-description">
-        Software Developer Intern at Copperleaf, enhancing UX with TypeScript and Angular. Microsoft TEALS TA,
-        teaching Python to high school students. Former Community Advisor at SFU and Mobile Developer at CJSF 90.1 FM.
-        President&apos;s Honour Roll recipient, committed to fostering inclusivity and innovation in tech.
+        Software Developer Intern at&nbsp;Copperleaf, enhancing&nbsp;UX with TypeScript and Angular. Microsoft TEALS&nbsp;TA,
+        teaching Python to&nbsp;high school students. Former Community Advisor at&nbsp;SFU and Mobile Developer at&nbsp;CJSF 90.1&nbsp;FM.
+        President&amp;apos;s Honour Roll recipient, committed to&nbsp;fostering inclusivity and innovation in&nbsp;tech.
       </p>
     </div>
   );
